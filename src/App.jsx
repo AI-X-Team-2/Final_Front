c0a08589dd3e03a97b354586d8b9a5864a41db45
Windows11 K <bungry@bu.ac.kr>
2025-08-01 16:55:42 +0900

    feat: game page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from './component/Layout'; // 'Rayout'은 오타이므로 제거
 import SpeechCorrection from './pages/SpeechCorrection';
+import Game from './pages/WordGame';
+import WordGame from './pages/WordGame';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         {/* Layout을 라우트에 감싸서 중첩 라우팅 적용 */}
         <Route element={<Layout />}>
           <Route path="/speech-correction" element={<SpeechCorrection />} />
+          <Route path="/game" element={<WordGame />} />
         </Route>
       </Routes>
     </BrowserRouter>
