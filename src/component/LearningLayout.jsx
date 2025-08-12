acd1cd676171313d0b320cda64cf8577e27a448f
seoyoungKim <seoyoungkim003@gmail.com>
2025-08-12 14:35:57 +0900

    fix:수정

---
+++ src/component/LearningLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const LearningLayout = () => {
+  return (
+   <div className="bg-customGray w-full max-w-[50rem] h-screen mx-auto flex flex-col">
+          
+            <div className="flex-1 pt-72 overflow-hidden">
+                <Outlet />
+            </div>
+        </div>
+  )
+}
+
+export default LearningLayout
