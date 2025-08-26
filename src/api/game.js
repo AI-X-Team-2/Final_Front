74bdd329cd46c0097607b8809ffc444f544c04f1
ZERO-NOA <158252452+ZERO-NOA@users.noreply.github.com>
2025-08-26 16:03:40 +0900

    000 fix

--- src/api/game.js
+++ src/api/game.js
@@ -6,7 +6,7 @@ const token = useAuthStore.getState()?.token;
 console.log(token)
 export async function createLeaderboardEntry(points) {
   const res = await axios.post(
-    "http://15.165.141.230/api/leaderboard/create",
+    "/api/leaderboard/create",
     { points },
     {
       headers: {
@@ -23,7 +23,7 @@ export async function createLeaderboardEntry(points) {
 export async function fetchLeaderboard() {
 
   const res = await axios.get(
-    "http://15.165.141.230/api/leaderboard/leaderboard",
+    "/api/leaderboard/leaderboard",
     {
       headers: {
         Authorization: `Bearer ${token}`, 
--- src/component/Audio.jsx
+++ src/component/Audio.jsx
@@ -132,7 +132,7 @@ const Audio = forwardRef(({
 
     try {
       const response = await axios.post(
-        "http://15.165.141.230/analyze",
+        "/analyze",
         formData,
         {
           headers: {
